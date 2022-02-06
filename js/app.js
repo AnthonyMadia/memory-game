70a59d92add5525f6545926d124fed71181be900
Anthony Madia <aamadia24@gmail.com>
2022-02-06 00:38:12 -0500

    set variables and event listeners

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
+const deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
-
+const difficulty = {
+  "easy": 5,
+  "medium": 10,
+  "hard": 15
+}
 
 /*---------------------------- Variables (state) ----------------------------*/
-// State variables:
-//   - cards [] 
-//   face down -> {'faceDown': 'cardVal}
-//   face up (not-matched yet) -> {'currentPick': 'cardVal'}
-//   match -> {'matched': 'cardVal'}
-//   - message
-//   - matchesRemaining 
-//   - turn 1/-1
-//   - seconds
-//   - tickInterval
-//   - waitingForTimeout (bool)
-//   - card1Idx/card1Val
-//   - card2Idx/card2Val
+let cards, message, matchesRemaining, turn, seconds, tickInterval, waitingForTimeout, card1Idx, card1Val, card2Idx, card2Val
+
 
 
 /*------------------------ Cached Element References ------------------------*/
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+const resetDiv = document.getElementById('reset-div')
+const difficultyBtns = document.getElementById('difficulty-buttons')
+const messageEl = document.getElementById('message')
+const playArea = document.getElementById('play-area')
+const resetBtn = document.getElementById('reset-button')
+
 
 
 /*-------------------------------- Functions --------------------------------*/

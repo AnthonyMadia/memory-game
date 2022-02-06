f0b7176df55905079664dd73c398ce67d6b8008c
Anthony Madia <aamadia24@gmail.com>
2022-02-06 12:23:49 -0500

    Add: intervals and compare functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/*-------------------------------- Constants --------------------------------*/
 const deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
 const difficulty = {
@@ -7,9 +6,9 @@ const difficulty = {
   "hard": 15
 }
 
+
 /*---------------------------- Variables (state) ----------------------------*/
 let cards, message, matchesRemaining, turn, seconds, tickInterval, waitingForTimeout, card1Idx, card1Val, card2Idx, card2Val
-
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -20,36 +19,42 @@ const playArea = document.getElementById('play-area')
 const resetBtn = document.getElementById('reset-button')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-difficultyBtns.addEventListener('click', function(evt) {
-  if (evt.target.classList.contains("btn")) {
+difficultyBtns.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('btn')) {
     difficultyBtns.classList.add("hidden")
     resetDiv.classList.remove("hidden")
     setDifficulty(difficulty[evt.target.id])
   }
 })
+
 resetBtn.addEventListener('click', init)
 
-playArea.addEventListener('click', function(evt) {
-  console.log(evt.target)
+playArea.addEventListener('click', function (evt) {
+  
+  if (
+    !isNaN(parseInt(evt.target.id)) &&
+    cards[parseInt(evt.target.id)]['faceDown'] && 
+    !waitingForTimeout
+  ) {
+    handleCardClick(parseInt(evt.target.id))
+  }
 })
 
 
 /*-------------------------------- Functions --------------------------------*/
+
 init()
 
 function init() {
-
   difficultyBtns.classList.remove("hidden")
   resetDiv.classList.add("hidden")
-  message = "Please Select Difficulty"
+  message = 'Please select difficulty:'
   waitingForTimeout = false
   turn = 1
-  playArea.innerHTML = "" // <- clears cards from playing field
+  playArea.innerHTML = ''
   cards = []
   matchesRemaining = 0
   seconds = 0
-  //interval
   clearInterval(tickInterval)
   tickInterval = setInterval(tick, 1000)
   render()
@@ -59,10 +64,40 @@ function tick() {
   seconds++
 }
 
-function handleCardClick() {
-  // if turn is 1 handle first card
-  // if turn is -1 handle the second card being picked
+function handleCardClick(cardIdx) {
+  if (turn === 1) {
+    message = 'Find the match!'
+    card1Idx = cardIdx
+    card1Val = cards[card1Idx]['faceDown']
+    cards[card1Idx] = {'currentPick': card1Val}
+  } else {
+    card2Idx = cardIdx
+    card2Val = cards[card2Idx]['faceDown']
+    cards[card2Idx] = {'currentPick': card2Val}
+    waitingForTimeout = true
+    setTimeout(function() {
+      compareCards(card1Val, card2Val)
+    }, 1000)
+  }
   turn *= -1
+  render()
+}
+
+function compareCards(card1Val, card2Val) {
+  if (card1Val === card2Val) {
+    matchesRemaining -= 1
+    message = `Nice work! ${matchesRemaining} pair${matchesRemaining !== 1 ? 's' : ''} left!`
+    if (matchesRemaining === 0) {
+      message = `Congratulations, you found all the matches in ${Math.floor(seconds/60)}:${seconds}`
+      clearInterval(tickInterval)
+    }
+  } else {
+    message = 'Try again!'
+    cards[card1Idx] = {'faceDown': card1Val}
+    cards[card2Idx] = {'faceDown': card2Val}
+  }
+  waitingForTimeout = false
+  render()
 }
 
 function setDifficulty(numCards) {
@@ -82,14 +117,14 @@ function shuffle(cardsIn) {
   let cardToShuffle 
   for (let i = 0; i = cardsIn.length; i++) {
     cardToShuffle = cardsIn.splice(Math.random() * cardsIn.length, 1)
-    cards.push({'cardDown': `${cardToShuffle}`})
+    cards.push({'faceDown': `${cardToShuffle}`})
   }
-  console.log(cards)
   render()
 }
 
 function render() {
   messageEl.textContent = message
+  playArea.innerHTML = ''
   let appendCard
   cards.forEach(function(card, idx) {
     appendCard = document.createElement("div")
@@ -104,8 +139,6 @@ function render() {
     playArea.appendChild(appendCard)
   })
 }
-
-
 // PsuedoCode:
 // Write init function
 //   - set state variables to initial state
